021ee35f742a77b7d82ef080bc5d863ce53d6a92
Samrath Singh Saini <62909539+SamrathSinghSaini@users.noreply.github.com>
2023-10-14 22:51:29 -0700

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
-let baseURL = 'https://ecommerce-store-backend-vop3.onrender.com'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
+let baseURL = "https://ecommerce-store-backend-vop3.onrender.com";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':baseURL,
-     
-    '/images':baseURL+'/images',
-    '/api/products':baseURL+'/api/products', 
-    '/api/category':baseURL+'/api/category', 
-    '/api/home':baseURL+'/api/home'
-    }
-  }, 
-  root:'.', 
-  build:{
-    outDir:'public'
-  }, 
-  publicDir:'public'
-})
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+
+    proxy: {
+      "/api": baseURL,
+
+      "/images": baseURL + "/images",
+      "/api/products": baseURL + "/api/products",
+      "/api/category": baseURL + "/api/category",
+      "/api/home": baseURL + "/api/home",
+    },
+  },
+  root: ".",
+  build: {
+    outDir: "public",
+  },
+  publicDir: "public",
+});
